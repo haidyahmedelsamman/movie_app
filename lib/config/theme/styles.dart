1cee5b008a6f4346896fe9c23dc3ce09ad26afa2
Haidy <haidyahmedelsamman@gmail.com>
2024-05-24 07:18:23 +0300

    Intialize onBoarding Screen + create app text button + added custom fonts + added colors file + added styles file + added extentions file + navigate between screens

---
+++ lib/config/theme/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app/config/theme/colors.dart';
+import 'font_weight_helper.dart';
+
+class TextStyles {
+  static TextStyle font18DarkGreyMedium = TextStyle(
+      fontSize: 18.sp,
+      fontWeight: FontWeightHelper.medium,
+      color: ColorsManager.darkGrey,
+      fontFamily: "Kanit");
+}
