114c4684ffefe64d7a37561301dcb4ef4ba6e43a
Haidy <haidyahmedelsamman@gmail.com>
2024-05-30 04:09:12 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:movie_app/movie_app.dart';
 import 'config/routes/app_router.dart';
 
 void main() {
+ // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+//   await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
   runApp(MovieApp(
     appRouter: AppRouter(),
   ));
 }
-
--- lib/movie_app.dart
+++ lib/movie_app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'config/routes/app_router.dart';
 import 'config/routes/routes.dart';
-
 
 class MovieApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -13,6 +13,7 @@ class MovieApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // FlutterNativeSplash.remove();
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
