d2a0b294c643df5cd8b60d8be992cace01b8851a
Haidy <haidyahmedelsamman@gmail.com>
2024-05-31 23:43:55 +0300

    integration with firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_app/firebase_options.dart';
 import 'package:movie_app/movie_app.dart';
 import 'config/routes/app_router.dart';
 
-void main() {
+Future<void> main() async {
  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-//   await Firebase.initializeApp(
-//   options: DefaultFirebaseOptions.currentPlatform,
-// );
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MovieApp(
     appRouter: AppRouter(),
   ));
